b132a09859d02b04a3ebf32b724da8db7adfef3d
febLey <mail@febley.me>
2026-01-27 17:32:25 +0100

    feat(plugins): show hidden files in snacks explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    explorer = {
+      hidden = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+        },
+      },
+    },
+  },
+}
